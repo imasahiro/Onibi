19dbe037b44509ab9f140ec7e17eb458c8372b52
Masahiro Ide <imasahiro9@gmail.com>
2026-08-10 22:50:33 +0900

    Support Unicode full case folding for literals (#50)

--- test/acceptance/case_folding_test.rb
+++ test/acceptance/case_folding_test.rb
@@ -6,4 +6,8 @@ class CaseFoldingTest < Minitest::Test
   def test_ignorecase_matches_unicode_simple_case_folding
     assert Onibi::Regexp.new("k", ["ignorecase"]).match?("K")
   end
+
+  def test_ignorecase_matches_unicode_full_case_folding
+    assert Onibi::Regexp.new("ß", ["ignorecase"]).match?("SS")
+  end
 end
