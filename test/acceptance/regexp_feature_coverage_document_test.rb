78e97c03b91dc94f4bec506721e5237cc44132f8
Masahiro Ide <imasahiro9@gmail.com>
2026-08-11 01:57:12 +0900

    Record regexp union option coverage

--- test/acceptance/regexp_feature_coverage_document_test.rb
+++ test/acceptance/regexp_feature_coverage_document_test.rb
@@ -19,5 +19,6 @@ class RegexpFeatureCoverageDocumentTest < Minitest::Test
     assert_includes document, "https://docs.ruby-lang.org/en/4.0/Regexp.html"
     assert_includes document, "https://docs.ruby-lang.org/en/4.0/MatchData.html"
     assert_includes document, "position 引数"
+    assert_includes document, "compiled pattern の ignorecase"
   end
 end
