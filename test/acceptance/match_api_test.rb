efcc251098820b2418c3c5e63492f94a8cb566c1
Masahiro Ide <imasahiro9@gmail.com>
2026-08-11 01:45:51 +0900

    Match unknown capture names with Ruby errors

--- test/acceptance/match_api_test.rb
+++ test/acceptance/match_api_test.rb
@@ -128,4 +128,14 @@ class MatchApiTest < Minitest::Test
     assert_raises(IndexError) { match.bytebegin(2) }
     assert_raises(TypeError) { match.match_length(nil) }
   end
+
+  def test_match_value_access_coerces_integer_like_indices_and_rejects_unknown_names
+    match = Onibi::Regexp.new("(?<animal>cat)").match("cat")
+
+    assert_equal "cat", match[0.9]
+    assert_equal ["cat"], match.values_at(0.9)
+    assert_raises(IndexError) { match["unknown"] }
+    assert_raises(IndexError) { match.values_at("unknown") }
+    assert_raises(TypeError) { match[nil] }
+  end
 end
