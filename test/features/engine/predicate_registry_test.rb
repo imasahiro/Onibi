5054dc83dd64c20a65cc18ac3ad2f462bca09db2
Masahiro Ide <imasahiro9@gmail.com>
2026-08-12 17:34:24 +0900

    Share equivalent ASCII predicate tables (#314)

--- test/features/engine/predicate_registry_test.rb
+++ test/features/engine/predicate_registry_test.rb
@@ -11,4 +11,20 @@ class PredicateRegistryTest < Minitest::Test
     assert_equal 0, registry.register(key)
     assert_equal [key], registry.entries
   end
+
+  def test_equivalent_ascii_ranges_share_one_index
+    registry = Onibi::Codegen::PredicateRegistry.new
+
+    assert_equal 0, registry.register(["a-c", false])
+    assert_equal 0, registry.register(["abc", false])
+    assert_equal [["a-c", false]], registry.entries
+  end
+
+  def test_equivalent_generated_classes_share_one_table
+    ast = Onibi::Parser.new("[a-c][abc]").parse
+    program = Onibi::Codegen::GeneratedProgram.ast(ast)
+
+    assert_equal ::Regexp.new("[a-c][abc]").match?("ab"), program.search("ab", 0, capture: false)
+    assert_equal 1, program.source.scan("TableRegistry.fetch").length
+  end
 end
