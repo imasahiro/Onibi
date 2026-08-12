7ca8525a4073201df252d650d449028a1a58f81a
Masahiro Ide <imasahiro9@gmail.com>
2026-08-12 14:33:14 +0900

    Short circuit empty candidate intersections

--- test/features/engine/candidate_source_test.rb
+++ test/features/engine/candidate_source_test.rb
@@ -18,4 +18,32 @@ class CandidateSourceTest < Minitest::Test
     assert_equal [3, 5], source.candidate_positions("input", 0)
     assert source.preserves_order?
   end
+
+  def test_intersection_stops_after_an_empty_source
+    first = CountingSource.new([])
+    second = CountingSource.new([1, 2, 3])
+
+    source = Onibi::Codegen::CandidateSource::Intersection.new([first, second])
+
+    assert_empty source.candidate_positions("input", 0)
+    assert_equal 1, first.calls
+    assert_equal 0, second.calls
+  end
+
+  class CountingSource
+    include Onibi::Codegen::CandidateSource
+    attr_reader :calls
+
+    def initialize(candidates)
+      @candidates = candidates
+      @calls = 0
+    end
+
+    def eligible?(_input, _position) = true
+
+    def candidate_positions(_input, _position)
+      @calls += 1
+      @candidates
+    end
+  end
 end
