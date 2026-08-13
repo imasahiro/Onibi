dd5415c0a218ca68f022311bba6f514ff523b365
Masahiro Ide <imasahiro9@gmail.com>
2026-08-13 09:15:23 +0900

    Run CI benchmarks for Onibi only

--- test/features/quality/benchmark_workflow_test.rb
+++ test/features/quality/benchmark_workflow_test.rb
@@ -8,4 +8,12 @@ class BenchmarkWorkflowTest < Minitest::Test
 
     assert_match(%r{pull_request:\n\s+paths:\n\s+- lib/\*\*}, workflow)
   end
+
+  def test_ci_benchmark_report_runs_only_onibi
+    report = File.read(File.join(PROJECT_ROOT, "script/benchmark_report.rb"))
+
+    assert_equal 2, report.scan(/benchmark\.report\("onibi"\)/).length
+    refute_match(/benchmark\.report\("ruby"\)/, report)
+    refute_match(/%i\[ruby onibi\]/, report)
+  end
 end
