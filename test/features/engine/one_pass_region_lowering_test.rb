7e35de4b6c5a11dee1ab530cb14003f83573c192
Masahiro Ide <imasahiro9@gmail.com>
2026-08-13 14:02:32 +0900

    Record one-pass region lowering (#344)

---
+++ test/features/engine/one_pass_region_lowering_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "../../test_helper"
+
+class OnePassRegionLoweringTest < Minitest::Test
+  def test_pipeline_records_one_pass_region_lowering
+    ast = Onibi::Parser.new("abc").parse
+    unit = Onibi::Codegen::Optimization::Pipeline.default.call(
+      ast, options: [], encoding: Encoding::UTF_8
+    )
+
+    assert_includes unit.applied_passes, :one_pass_region_lowering
+    assert_equal 1, unit.cfg.blocks.length
+  end
+end
