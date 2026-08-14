ac889cbe5d722842c7739cd50a96d8e5f95fd0fe
Masahiro Ide <imasahiro9@gmail.com>
2026-08-14 12:54:00 +0900

    Prioritize match reset HFA dispatch

--- lib/onibi.rb
+++ lib/onibi.rb
@@ -191,6 +191,10 @@ module Onibi
         normalized_position = position.is_a?(Integer) && position.zero? ? 0 : normalize_match_position(input, position)
         return hfa_captured_class_run_chain_match?(input, normalized_position)
       end
+      if input.ascii_only? && @hfa_match_reset_literal_fast
+        normalized_position = position.is_a?(Integer) && position.zero? ? 0 : normalize_match_position(input, position)
+        return !input.index(@hfa_match_reset_literal_fast, normalized_position).nil?
+      end
       if input.ascii_only? && @hfa_literal_assertion_fast
         normalized_position = position.is_a?(Integer) && position.zero? ? 0 : normalize_match_position(input, position)
         return !hfa_literal_assertion_match_result(input, normalized_position, @hfa_literal_assertion_fast).nil?
@@ -348,10 +352,6 @@ module Onibi
       if input.ascii_only? && hfa_captured_class_run_chain_result_safe?
         normalized_position = position.is_a?(Integer) && position.zero? ? 0 : normalize_match_position(input, position)
         return hfa_captured_class_run_chain_match?(input, normalized_position)
-      end
-      if input.ascii_only? && @hfa_match_reset_literal_fast
-        normalized_position = position.is_a?(Integer) && position.zero? ? 0 : normalize_match_position(input, position)
-        return !input.index(@hfa_match_reset_literal_fast, normalized_position).nil?
       end
       if input.ascii_only? && (literal = hfa_match_reset_literal_combined_literal)
         normalized_position = position.is_a?(Integer) && position.zero? ? 0 : normalize_match_position(input, position)
