29e40aedf5b5b69caabf27e6373579814dd865bb
Masahiro Ide <imasahiro9@gmail.com>
2026-08-14 10:37:32 +0900

    Route positive lookahead scans through HFA

--- test/features/matching/scan_gsub_test.rb
+++ test/features/matching/scan_gsub_test.rb
@@ -91,6 +91,14 @@ class ScanGsubTest < Minitest::Test
     end
   end
 
+  def test_literal_positive_lookahead_scan_uses_hfa_iterator
+    regexp = Onibi::Regexp.new("a(?=b)")
+
+    regexp.stub(:codegen_each_result, ->(*) { flunk "literal positive lookahead should use HFA" }) do
+      assert_equal %w[a a], regexp.scan("ab ac ab")
+    end
+  end
+
   def test_class_run_capture_scan_uses_hfa_iterator
     regexp = Onibi::Regexp.new("([a-z]+)-([0-9]+)")
 
