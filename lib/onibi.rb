ce30a6b5658c0897cf8e826019fc0a2d412ddf4f
Masahiro Ide <imasahiro9@gmail.com>
2026-08-15 21:00:53 +0900

    Remove ASCII class run scan route

--- lib/onibi.rb
+++ lib/onibi.rb
@@ -5265,14 +5265,6 @@ module Onibi
         end
         return true
       end
-      if ascii_input && hfa_ascii_class_run_result_safe?
-        position = 0
-        while (result = hfa_ascii_class_run_match_result(input, position))
-          block.call(result)
-          position = result[1]
-        end
-        return true
-      end
       if ascii_input && hfa_captured_class_run_chain_result_safe?
         position = 0
         while (result = hfa_captured_class_run_chain_match_result(input, position))
