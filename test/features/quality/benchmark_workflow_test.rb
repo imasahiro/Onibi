5d68f49ac63c54a627740c9eaca12ba3a645b584
Masahiro Ide <imasahiro9@gmail.com>
2026-08-15 13:06:18 +0900

    Stabilize benchmark comparison timing

---
+++ test/features/quality/benchmark_workflow_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class BenchmarkWorkflowTest < Minitest::Test
+  def test_benchmark_workflow_uses_stable_measurement_window
+    workflow = File.read(File.join(PROJECT_ROOT, ".github/workflows/benchmark.yml"))
+
+    assert_equal 2, workflow.scan(%r{script/benchmark_report\.rb --time 2}).length
+    assert_equal 2, workflow.scan(/--warmup 1/).length
+  end
+end
