dc2bbd3d26afd21dfa577d879fae6d30604969c6
Masahiro Ide <imasahiro9@gmail.com>
2026-08-15 14:40:32 +0900

    Remove non-library quality tests

--- test/features/quality/coverage_configuration_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../test_helper"
-
-class CoverageConfigurationTest < Minitest::Test
-  def test_simplecov_is_configured_for_the_test_suite
-    assert_equal PROJECT_ROOT, SimpleCov.root
-  end
-end
