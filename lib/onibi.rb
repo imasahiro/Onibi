ff1af3b609c5c9104cb7e171a2e45585cc8bdea1
Masahiro Ide <imasahiro9@gmail.com>
2026-08-20 16:50:53 +0900

    Remove ASCII ignorecase match route

--- lib/onibi.rb
+++ lib/onibi.rb
@@ -291,13 +291,6 @@ module Onibi
         return nil
       end
 
-      if ascii_input && hfa_ignorecase_literal_result_safe?
-        result = hfa_ignorecase_literal_match_result(input, normalized_position)
-        return hfa_match_data(result, input) if result
-
-        return nil
-      end
-
       if (spec = hfa_bounded_sequence_direct_spec) && (ascii_input || spec[:table].nil?)
         result = hfa_bounded_sequence_direct_match_result(input, normalized_position)
         if result
