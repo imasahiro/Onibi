c67b85a23bc893da0c13d53ed29334dd071ded38
Masahiro Ide <imasahiro9@gmail.com>
2026-08-22 12:16:20 +0900

    Test atomic absence and scoped option AST grammar

---
+++ test/features/v2/parser_advanced_groups_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class V2ParserAdvancedGroupsTest < Minitest::Test
+  def test_atomic_group_has_exact_alternation_body
+    body = Onibi::AST::Alternation.new([
+                                         Onibi::AST::Sequence.new([Onibi::AST::Literal.new("a")]),
+                                         Onibi::AST::Sequence.new([Onibi::AST::Literal.new("a"), Onibi::AST::Literal.new("b")])
+                                       ])
+
+    assert_equal Onibi::AST::Sequence.new([Onibi::AST::AtomicGroup.new(body)]),
+                 Onibi::V2::Parser.parse("(?>a|ab)").ast
+  end
+
+  def test_absence_group_has_exact_nested_body
+    body = Onibi::AST::Sequence.new([
+                                      Onibi::AST::Group.new(
+                                        Onibi::AST::Sequence.new([Onibi::AST::Literal.new("a")]), 1, true, nil
+                                      )
+                                    ])
+
+    assert_equal Onibi::AST::Sequence.new([Onibi::AST::Absence.new(body)]),
+                 Onibi::V2::Parser.parse("(?~(a))").ast
+  end
+
+  def test_scoped_option_group_has_exact_flags
+    body = Onibi::AST::Sequence.new(%w[c a t].map { |value| Onibi::AST::Literal.new(value) })
+    expected = Onibi::AST::Sequence.new([Onibi::AST::OptionGroup.new(body, true, nil, nil)])
+
+    assert_equal expected, Onibi::V2::Parser.parse("(?i:cat)").ast
+  end
+end
