26f36b1317117c8255a22e2f5483cafa3f0b50c6
Masahiro Ide <imasahiro9@gmail.com>
2026-08-22 12:27:18 +0900

    Make quantifier literal expectations explicit

--- test/features/v2/parser_quantifiers_test.rb
+++ test/features/v2/parser_quantifiers_test.rb
@@ -5,9 +5,9 @@ require "test_helper"
 class V2ParserQuantifiersTest < Minitest::Test
   def test_short_quantifiers_have_exact_bounds_and_modes
     expected = Onibi::AST::Sequence.new([
-                                          quantifier(:"?", 0, 1, :greedy),
-                                          quantifier(:+, 1, nil, :lazy),
-                                          quantifier(:+, 1, nil, :possessive)
+                                          quantifier("a", :"?", 0, 1, :greedy),
+                                          quantifier("a", :+, 1, nil, :lazy),
+                                          quantifier("a", :+, 1, nil, :possessive)
                                         ])
 
     assert_equal expected, Onibi::V2::Parser.parse("a?a+?a++").ast
@@ -15,9 +15,9 @@ class V2ParserQuantifiersTest < Minitest::Test
 
   def test_bounded_quantifiers_have_exact_bounds
     expected = Onibi::AST::Sequence.new([
-                                          quantifier(:bounded, 2, 4, :greedy),
-                                          quantifier(:bounded, 2, nil, :greedy),
-                                          quantifier(:bounded, 2, 2, :greedy)
+                                          quantifier("a", :bounded, 2, 4, :greedy),
+                                          quantifier("a", :bounded, 2, nil, :greedy),
+                                          quantifier("a", :bounded, 2, 2, :greedy)
                                         ])
 
     assert_equal expected, Onibi::V2::Parser.parse("a{2,4}a{2,}a{2}").ast
@@ -25,7 +25,8 @@ class V2ParserQuantifiersTest < Minitest::Test
 
   private
 
-  def quantifier(kind, minimum, maximum, mode)
-    Onibi::AST::Quantifier.new(Onibi::AST::Literal.new("a"), kind, minimum, maximum, mode)
+  def quantifier(value, kind, minimum, maximum, mode)
+    expression = Onibi::AST::Literal.new(value)
+    Onibi::AST::Quantifier.new(expression, kind, minimum, maximum, mode)
   end
 end
