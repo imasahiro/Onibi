04518758759b79740e6b374ce395aa328f2f1094
Masahiro Ide <imasahiro9@gmail.com>
2026-08-22 12:19:14 +0900

    Test pattern comment AST parsing

---
+++ test/features/v2/parser_comments_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class V2ParserCommentsTest < Minitest::Test
+  def test_pattern_comment_is_removed_without_changing_ast
+    expected = Onibi::AST::Sequence.new([
+                                          Onibi::AST::Literal.new("c"),
+                                          Onibi::AST::Literal.new("a"),
+                                          Onibi::AST::Literal.new("t")
+                                        ])
+
+    assert_equal expected, Onibi::V2::Parser.parse("(?# greeting)cat").ast
+  end
+end
