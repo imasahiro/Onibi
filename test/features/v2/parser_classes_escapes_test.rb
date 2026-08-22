d2b29cec4a71c7966e5f9f66a780ba59609a3633
Masahiro Ide <imasahiro9@gmail.com>
2026-08-22 12:15:01 +0900

    Test character class and escape AST grammar

---
+++ test/features/v2/parser_classes_escapes_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class V2ParserClassesEscapesTest < Minitest::Test
+  def test_character_classes_have_exact_source_values
+    expected = Onibi::AST::Sequence.new([
+                                          Onibi::AST::CharacterClass.new("a-z"),
+                                          Onibi::AST::CharacterClass.new("^0-9"),
+                                          Onibi::AST::CharacterClass.new("[:digit:]")
+                                        ])
+
+    assert_equal expected, Onibi::V2::Parser.parse("[a-z][^0-9][[:digit:]]").ast
+  end
+
+  def test_escape_nodes_have_exact_kinds
+    expected = Onibi::AST::Sequence.new([
+                                          Onibi::AST::Escape.new(:digit),
+                                          Onibi::AST::Escape.new(:not_digit),
+                                          Onibi::AST::Escape.new(:linebreak),
+                                          Onibi::AST::Escape.new(:word_boundary)
+                                        ])
+
+    assert_equal expected, Onibi::V2::Parser.parse("\\d\\D\\R\\b").ast
+  end
+
+  def test_property_and_encoded_escape_nodes_have_exact_values
+    expected = Onibi::AST::Sequence.new([
+                                          Onibi::AST::Property.new("Alpha", false),
+                                          Onibi::AST::Literal.new("A"),
+                                          Onibi::AST::Literal.new("A"),
+                                          Onibi::AST::Literal.new("A")
+                                        ])
+
+    assert_equal expected, Onibi::V2::Parser.parse("\\p{Alpha}\\u0041\\x41\\101").ast
+  end
+end
