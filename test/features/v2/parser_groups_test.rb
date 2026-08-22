14b954e972c6f412302ce5be6cc5b944f23c2eb8
Masahiro Ide <imasahiro9@gmail.com>
2026-08-22 12:14:03 +0900

    Test capture group AST grammar

---
+++ test/features/v2/parser_groups_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class V2ParserGroupsTest < Minitest::Test
+  def test_numbered_and_non_capturing_groups_have_exact_ast
+    expected = Onibi::AST::Sequence.new([
+                                          Onibi::AST::Group.new(
+                                            Onibi::AST::Sequence.new([Onibi::AST::Literal.new("a"), Onibi::AST::Literal.new("b")]), 1, true, nil
+                                          ),
+                                          Onibi::AST::Group.new(
+                                            Onibi::AST::Sequence.new([Onibi::AST::Literal.new("c"), Onibi::AST::Literal.new("d")]), nil, false, nil
+                                          )
+                                        ])
+
+    assert_equal expected, Onibi::V2::Parser.parse("(ab)(?:cd)").ast
+  end
+
+  def test_named_group_has_name_and_capture_number
+    body = Onibi::AST::Sequence.new([Onibi::AST::Literal.new("a")])
+    expected = Onibi::AST::Sequence.new([Onibi::AST::Group.new(body, 1, true, "word")])
+
+    assert_equal expected, Onibi::V2::Parser.parse("(?<word>a)").ast
+  end
+end
