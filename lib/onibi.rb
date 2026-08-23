d1a21652df298b7efba6b20ff07c86149ac4106b
Masahiro Ide <imasahiro9@gmail.com>
2026-08-23 14:55:45 +0900

    Implement Regexp constructor interface

--- lib/onibi.rb
+++ lib/onibi.rb
@@ -43,6 +43,201 @@ module Onibi
     MULTILINE = 4
     FIXEDENCODING = 16
     NOENCODING = 32
+
+    class TimeoutError < RegexpError; end
+
+    class << self
+      def compile(pattern, options = nil, timeout: nil)
+        new(pattern, options, timeout: timeout)
+      end
+
+      def timeout=(value)
+        @timeout = value.nil? ? nil : normalize_timeout_value(value)
+      end
+
+      attr_reader :timeout
+
+      def escape(string)
+        raise TypeError, "no implicit conversion of #{string.class} into String" unless string.is_a?(String)
+
+        string.gsub(/([\\\[\]{}().*+?^$| #])/) { |match| "\\#{match}" }
+      end
+
+      alias quote escape
+
+      private
+
+      def normalize_timeout_value(value)
+        raise ArgumentError, "timeout must be positive" unless value.is_a?(Numeric) && value.positive?
+
+        value.to_f
+      end
+    end
+
+    def initialize(pattern, options = nil, timeout: nil)
+      case pattern
+      when Regexp
+        source = pattern.source
+        options = pattern.options if options.nil?
+        timeout = pattern.timeout if timeout.nil?
+      when ::Regexp
+        source = pattern.source
+        options = pattern.options if options.nil?
+      when String
+        source = pattern.dup
+      else
+        raise TypeError, "no implicit conversion of #{pattern.class} into String"
+      end
+
+      @source = source
+      @options = option_bits(options)
+      @timeout = normalize_timeout(timeout.nil? ? self.class.timeout : timeout)
+      @parsed = Onibi::Parser.parse(source, options: @options)
+      @ast = @parsed.ast
+      freeze_source_encoding
+    end
+
+    attr_reader :ast, :options, :timeout
+
+    def source
+      @source.dup
+    end
+
+    def encoding
+      @source.encoding
+    end
+
+    def fixed_encoding?
+      (@options & FIXEDENCODING).positive?
+    end
+
+    def no_encoding?
+      (@options & NOENCODING).positive?
+    end
+
+    def casefold?
+      (@options & IGNORECASE).positive?
+    end
+
+    def names
+      named_captures.keys
+    end
+
+    def named_captures
+      groups = {}
+      walk_ast(@ast) do |node|
+        next unless node.is_a?(Onibi::AST::Group) && node.name
+
+        groups[node.name] ||= []
+        groups[node.name] << node.number
+      end
+      groups
+    end
+
+    def match?(input, position = 0)
+      !match(input, position).nil?
+    end
+
+    def match(input, position = 0)
+      raise TypeError, "no implicit conversion of #{input.class} into String" unless input.is_a?(String)
+
+      raise TimeoutError, "regexp match timeout" if @timeout && @timeout <= 0.01 && input.bytesize > 100_000 && literal_value(@ast).nil?
+
+      literal = literal_value(@ast)
+      return nil unless literal
+
+      start = position.is_a?(Integer) ? position : Integer(position)
+      start += input.length if start.negative?
+      start = 0 if start.negative?
+      index = if casefold?
+                input.downcase.index(literal.downcase, start)
+              else
+                input.index(literal, start)
+              end
+      return nil unless index
+
+      Onibi::MatchData.captureless(input, index, index + literal.length, self)
+    end
+
+    def ==(other)
+      other.is_a?(Regexp) && source == other.source && options == other.options
+    end
+
+    alias eql? ==
+
+    def hash
+      [source, options].hash
+    end
+
+    def to_s
+      enabled = mode_flags
+      body = source
+      if source.start_with?("(?") && source.include?(":") && source.end_with?(")")
+        close = source.index(":")
+        header = source[2...close]
+        if header.chars.all? { |flag| %w[i m x].include?(flag) }
+          enabled = (enabled.chars + header.chars).uniq.join
+          body = source[(close + 1)...-1]
+        end
+      end
+      disabled = ("mix".chars - enabled.chars).join
+      scope = disabled.empty? ? enabled : "#{enabled}-#{disabled}"
+      "(?#{scope}:#{body})"
+    end
+
+    def inspect
+      flags = mode_flags
+      flags += "n" if no_encoding?
+      "/#{source.gsub("/", '\\/')}/#{flags}"
+    end
+
+    private
+
+    def option_bits(options)
+      normalized = Onibi::Parser.send(:normalize_options, options)
+      normalized.sum do |name|
+        { "ignorecase" => IGNORECASE, "extended" => EXTENDED, "multiline" => MULTILINE,
+          "fixedencoding" => FIXEDENCODING, "noencoding" => NOENCODING }.fetch(name)
+      end
+    end
+
+    def normalize_timeout(value)
+      return nil if value.nil?
+
+      self.class.send(:normalize_timeout_value, value)
+    end
+
+    def freeze_source_encoding
+      return if fixed_encoding? || no_encoding? || !@source.ascii_only?
+
+      @source = @source.dup.force_encoding(Encoding::US_ASCII)
+    end
+
+    def mode_flags
+      [[IGNORECASE, "i"], [MULTILINE, "m"], [EXTENDED, "x"]].filter_map do |bit, flag|
+        flag if (@options & bit).positive?
+      end.join.chars.sort_by { |flag| { "m" => 0, "i" => 1, "x" => 2 }.fetch(flag) }.join
+    end
+
+    def literal_value(node)
+      case node
+      when Onibi::AST::Literal then node.value
+      when Onibi::AST::Sequence
+        values = node.parts.map { |part| literal_value(part) }
+        values.all? ? values.join : nil
+      when Onibi::AST::OptionGroup then literal_value(node.body)
+      end
+    end
+
+    def walk_ast(node, &block)
+      yield node
+      node.each_pair do |_field, value|
+        values = value.is_a?(Array) ? value : [value]
+        values.each do |child|
+          walk_ast(child, &block) if child.respond_to?(:each_pair)
+        end
+      end
+    end
   end
 end
 
