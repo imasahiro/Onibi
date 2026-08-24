d667b2a57341777bb6018ebd5a335b02dadbaab1
Masahiro Ide <imasahiro9@gmail.com>
2026-08-25 08:24:58 +0900

    Support MRI Grapheme Extend property

--- lib/onibi/unicode_properties.rb
+++ lib/onibi/unicode_properties.rb
@@ -13,7 +13,7 @@ module Onibi
     SUPPORTED = %w[
       ASCII Any Alphabetic Bidi_Control Case_Ignorable Decimal_Number Default_Ignorable_Code_Point Deprecated Diacritic Emoji Emoji_Component
       Emoji_Presentation Emoji_Modifier Join_Control Noncharacter_Code_Point Pattern_White_Space Quotation_Mark
-      Cased Extender ID_Compat_Math_Continue ID_Compat_Math_Start Ideographic Lowercase Math Pattern_Syntax
+      Cased Extender Grapheme_Extend ID_Compat_Math_Continue ID_Compat_Math_Start Ideographic Lowercase Math Pattern_Syntax
       Prepended_Concatenation_Mark Punctuation Regional_Indicator Sentence_Terminal Soft_Dotted
       Terminal_Punctuation Unified_Ideograph Uppercase Variation_Selector
       Emoji_Modifier_Base Extended_Pictographic Han Hiragana Katakana Latin Greek Cyrillic Arabic
@@ -54,6 +54,7 @@ module Onibi
       "Unified_Ideograph" => :unified_ideograph?,
       "ID_Compat_Math_Start" => :id_compat_math_start?,
       "ID_Compat_Math_Continue" => :id_compat_math_continue?,
+      "Grapheme_Extend" => :grapheme_extend?,
       "Pattern_Syntax" => :pattern_syntax?,
       "Emoji_Presentation" => :emoji_presentation?, "Emoji_Modifier" => :emoji_modifier?,
       "Emoji_Modifier_Base" => :emoji_modifier_base?,
