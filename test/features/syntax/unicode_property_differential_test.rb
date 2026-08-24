15ad2124673e0fa1f55c3fbb96039f7e915d71eb
Masahiro Ide <imasahiro9@gmail.com>
2026-08-24 22:29:06 +0900

    Support Unicode Bassa Vah block

--- test/features/syntax/unicode_property_differential_test.rb
+++ test/features/syntax/unicode_property_differential_test.rb
@@ -82,6 +82,7 @@ class UnicodePropertyDifferentialTest < Minitest::Test
     ["\\p{InMedefaidrin}", ["\u{16E40}", "\u{16E9F}"], %w[A अ]],
     ["\\p{InMende_Kikakui}", ["\u{1E800}", "\u{1E8DF}"], %w[A अ]],
     ["\\p{InNyiakeng_Puachue_Hmong}", ["\u{1E100}", "\u{1E14F}"], %w[A अ]],
+    ["\\p{InBassa_Vah}", ["\u{16AD0}", "\u{16AFF}"], %w[A अ]],
     ["\\p{InCJK_Strokes}", ["㇐", "㇑"], %w[A あ]],
     ["\\p{InIdeographic_Description_Characters}", ["⿰", "⿻"], %w[A あ]],
     ["\\p{InCJK_Radicals_Supplement}", ["⺀", "⻳"], %w[A あ]],
