bd5db78be3478499bfe72513d11879f10ef40a5b
Masahiro Ide <imasahiro9@gmail.com>
2026-08-24 22:17:27 +0900

    Support Unicode Devanagari Extended A block

--- lib/onibi/unicode_property_scripts.rb
+++ lib/onibi/unicode_property_scripts.rb
@@ -86,6 +86,7 @@ UNICODE_LATIN_RANGES = [[65, 90], [97, 122], [170, 170], [186, 186], [192, 214],
       "Arabic_Extended_C" => [[0x10EC0, 0x10EFF]],
       "Arabic_Mathematical_Alphabetic_Symbols" => [[0x1EE00, 0x1EEFF]],
       "Devanagari_Extended" => [[0xA8E0, 0xA8FF]],
+      "Devanagari_Extended_A" => [[0x11B00, 0x11B5F]],
       "Arabic_Presentation_Forms_B" => [[0xFE70, 0xFEFF]]
     }.freeze
 
