eb9be262ed1be930ac3b00a5c1041a8a661f3fed
Masahiro Ide <imasahiro9@gmail.com>
2026-08-24 22:24:22 +0900

    Support Unicode Toto block

--- test/features/syntax/unicode_property_differential_test.rb
+++ test/features/syntax/unicode_property_differential_test.rb
@@ -77,6 +77,7 @@ class UnicodePropertyDifferentialTest < Minitest::Test
     ["\\p{InOl_Onal}", ["\u{1E5D0}", "\u{1E5FF}"], %w[A अ]],
     ["\\p{InTangsa}", ["\u{16A70}", "\u{16ACF}"], %w[A अ]],
     ["\\p{InWancho}", ["\u{1E2C0}", "\u{1E2FF}"], %w[A अ]],
+    ["\\p{InToto}", ["\u{1E290}", "\u{1E2BF}"], %w[A अ]],
     ["\\p{InCJK_Strokes}", ["㇐", "㇑"], %w[A あ]],
     ["\\p{InIdeographic_Description_Characters}", ["⿰", "⿻"], %w[A あ]],
     ["\\p{InCJK_Radicals_Supplement}", ["⺀", "⻳"], %w[A あ]],
