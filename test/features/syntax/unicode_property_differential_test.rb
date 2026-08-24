0d48c72dbda43e468553ee97269313228ff2e340
Masahiro Ide <imasahiro9@gmail.com>
2026-08-25 07:03:20 +0900

    Support MRI Case Ignorable property

--- test/features/syntax/unicode_property_differential_test.rb
+++ test/features/syntax/unicode_property_differential_test.rb
@@ -15,6 +15,7 @@ class UnicodePropertyDifferentialTest < Minitest::Test
     ["\\p{Emoji_Modifier_Base}", ["👍", "🧑"], %w[A 😀]],
     ["\\p{Extended_Pictographic}", %w[© 😀 🀄], %w[A あ]],
     ["\\p{Bidi_Control}", ["\u061c", "\u200e", "\u202e"], %w[A あ]],
+    ["\\p{Case_Ignorable}", ["'", "\u0301", "\u200c"], %w[A あ]],
     ["\\p{Han}", %w[漢 𠀀], ["あ"]],
     ["\\p{Latin}", %w[A é ꝑ], ["Ж"]],
     ["\\p{Greek}", %w[Ω ἂ], ["A"]],
