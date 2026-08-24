409c3a48c407a3581372e0b695c8dd15109d12ff
Masahiro Ide <imasahiro9@gmail.com>
2026-08-24 22:34:08 +0900

    Support Unicode Pahawh Hmong block

--- test/features/syntax/unicode_property_differential_test.rb
+++ test/features/syntax/unicode_property_differential_test.rb
@@ -85,6 +85,7 @@ class UnicodePropertyDifferentialTest < Minitest::Test
     ["\\p{InBassa_Vah}", ["\u{16AD0}", "\u{16AFF}"], %w[A अ]],
     ["\\p{InMiao}", %w[𖼀 𖾟], %w[A अ]],
     ["\\p{InBamum_Supplement}", ["𖠀", "𖨿"], %w[A अ]],
+    ["\\p{InPahawh_Hmong}", %w[𖬀 𖮏], %w[A अ]],
     ["\\p{InCJK_Strokes}", ["㇐", "㇑"], %w[A あ]],
     ["\\p{InIdeographic_Description_Characters}", ["⿰", "⿻"], %w[A あ]],
     ["\\p{InCJK_Radicals_Supplement}", ["⺀", "⻳"], %w[A あ]],
