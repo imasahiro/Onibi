d7c2508b3bc6d8c47e49e1e69209e2cab62cdef5
Masahiro Ide <imasahiro9@gmail.com>
2026-08-25 08:14:52 +0900

    Support MRI Prepended Concatenation Mark property

--- lib/onibi/unicode_properties.rb
+++ lib/onibi/unicode_properties.rb
@@ -13,7 +13,7 @@ module Onibi
     SUPPORTED = %w[
       ASCII Any Bidi_Control Case_Ignorable Default_Ignorable_Code_Point Deprecated Diacritic Emoji Emoji_Component
       Emoji_Presentation Emoji_Modifier Join_Control Noncharacter_Code_Point Pattern_White_Space Quotation_Mark
-      Cased Extender Math Regional_Indicator Sentence_Terminal Soft_Dotted Terminal_Punctuation Variation_Selector
+      Cased Extender Math Prepended_Concatenation_Mark Regional_Indicator Sentence_Terminal Soft_Dotted Terminal_Punctuation Variation_Selector
       Emoji_Modifier_Base Extended_Pictographic Han Hiragana Katakana Latin Greek Cyrillic Arabic
       Alpha Letter Alnum Digit Nd Number Lower Upper Space Word
       XDigit Hex_Digit Dash ASCII_Hex_Digit Assigned White_Space Blank Cntrl Graph Print Punct
@@ -47,6 +47,7 @@ module Onibi
       "Math" => :math?,
       "Cased" => :cased?,
       "Sentence_Terminal" => :sentence_terminal?,
+      "Prepended_Concatenation_Mark" => :prepended_concatenation_mark?,
       "Emoji_Presentation" => :emoji_presentation?, "Emoji_Modifier" => :emoji_modifier?,
       "Emoji_Modifier_Base" => :emoji_modifier_base?,
       "Extended_Pictographic" => :extended_pictographic?,
--- test/features/syntax/unicode_property_differential_test.rb
+++ test/features/syntax/unicode_property_differential_test.rb
@@ -32,6 +32,7 @@ class UnicodePropertyDifferentialTest < Minitest::Test
     ["\\p{Math}", ["+", "<", "∑", "∞", "𝑖"], %w[A あ 😀]],
     ["\\p{Cased}", %w[A a Ω Ж], %w[1 あ 😀]],
     ["\\p{Sentence_Terminal}", [".", "!", "?", "。", "؟"], %w[A あ 😀]],
+    ["\\p{Prepended_Concatenation_Mark}", ["\u0600", "\u0601", "\u06dd"], %w[A あ 😀]],
     ["\\p{Han}", %w[漢 𠀀], ["あ"]],
     ["\\p{Latin}", %w[A é ꝑ], ["Ж"]],
     ["\\p{Greek}", %w[Ω ἂ], ["A"]],
