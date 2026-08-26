8399742fc337f63406f11b00acae33cb1fa33151
Masahiro Ide <imasahiro9@gmail.com>
2026-08-26 23:27:21 +0900

    Cover bounded scoped property suffixes

--- test/features/v2/interpreter_test.rb
+++ test/features/v2/interpreter_test.rb
@@ -628,6 +628,15 @@ class InterpreterTest < Minitest::Test
                  regexp.match("x")&.to_a
   end
 
+  def test_scoped_unicode_property_bounded_quantifier_ascii_suffix_uses_flat_vm
+    regexp = Onibi::Regexp.new("(?i:\\p{Letter}){2,3}x")
+    program = regexp.send(:bytecode_program)
+
+    refute(program.instructions.any? { |instruction| instruction.opcode == :semantic_match })
+    assert_equal ::Regexp.new("(?i:\\p{Letter}){2,3}x").match("ſſſx")&.to_a,
+                 regexp.match("ſſſx")&.to_a
+  end
+
   def test_noencoding_byte_escape_uses_flat_vm
     regexp = Onibi::Regexp.new("\\xFF", Onibi::Regexp::NOENCODING)
     program = regexp.send(:bytecode_program)
