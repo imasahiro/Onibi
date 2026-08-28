c7443ac245618969b18e68d65b3de74fd1c36ee8
Masahiro Ide <imasahiro9@gmail.com>
2026-08-29 02:33:51 +0900

    Fold capture detection into AST nullability scan

--- ext/onibi/onibi.c
+++ ext/onibi/onibi.c
@@ -1269,7 +1269,7 @@ static inline OnibiParsed *onibi_parsed_get(VALUE value) {
 
 static int onibi_ast_safe_multibyte_class(VALUE ast);
 static int onibi_ast_anchor_repeat(VALUE ast);
-static int onibi_ast_nullable_scan(VALUE ast, int *nullable_capture, int *nullable_absence);
+static int onibi_ast_nullable_scan(VALUE ast, int *nullable_capture, int *nullable_absence, int *has_capture);
 
 static VALUE onibi_parser_parse_internal(VALUE source, VALUE options, VALUE supplied_tokens) {
   source = StringValue(source);
@@ -1289,7 +1289,8 @@ static VALUE onibi_parser_parse_internal(VALUE source, VALUE options, VALUE supp
   if (onibi_ast_anchor_repeat(parsed->ast)) parsed->ast_flags |= ONIBI_AST_FLAG_ANCHOR_REPEAT;
   int nullable_capture = 0;
   int nullable_absence = 0;
-  (void)onibi_ast_nullable_scan(parsed->ast, &nullable_capture, &nullable_absence);
+  int has_capture = 0;
+  (void)onibi_ast_nullable_scan(parsed->ast, &nullable_capture, &nullable_absence, &has_capture);
   if (nullable_absence) parsed->ast_flags |= ONIBI_AST_FLAG_NULLABLE_ABSENCE;
   if (nullable_capture) parsed->ast_flags |= ONIBI_AST_FLAG_NULLABLE_CAPTURE;
   return result;
@@ -3727,34 +3728,38 @@ static int onibi_ast_anchor_repeat(VALUE ast) {
   return (onibi_ast_anchor_scan(ast) & 2) != 0;
 }
 
-static int onibi_ast_nullable_scan(VALUE ast, int *nullable_capture, int *nullable_absence) {
+static int onibi_ast_nullable_scan(VALUE ast, int *nullable_capture, int *nullable_absence, int *has_capture) {
   if (NIL_P(ast)) return 1;
   if (RB_TYPE_P(ast, T_ARRAY)) {
     int result = 1;
     for (long i = 0; i < RARRAY_LEN(ast); i++)
-      if (!onibi_ast_nullable_scan(rb_ary_entry(ast, i), nullable_capture, nullable_absence)) result = 0;
+      if (!onibi_ast_nullable_scan(rb_ary_entry(ast, i), nullable_capture, nullable_absence, has_capture)) result = 0;
     return result;
   }
   if (!RB_TYPE_P(ast, T_HASH)) return 1;
   OnibiAstKind type = onibi_ast_kind(ast);
+  if (type == ONIBI_AST_CAPTURE) *has_capture = 1;
   if (type == ONIBI_AST_CAPTURE) {
-    int body_nullable = onibi_ast_nullable_scan(onibi_hash_value_id(ast, id_key_body), nullable_capture, nullable_absence);
+    int body_nullable = onibi_ast_nullable_scan(onibi_hash_value_id(ast, id_key_body), nullable_capture, nullable_absence, has_capture);
     if (body_nullable) *nullable_capture = 1;
     return body_nullable;
   }
   if (type == ONIBI_AST_QUANTIFIER) {
     VALUE atom = onibi_hash_value_id(ast, id_key_atom);
-    int atom_nullable = onibi_ast_nullable_scan(atom, nullable_capture, nullable_absence);
+    int atom_has_capture = 0;
+    int atom_nullable = onibi_ast_nullable_scan(atom, nullable_capture, nullable_absence, &atom_has_capture);
+    if (atom_has_capture) *has_capture = 1;
     VALUE min = onibi_hash_value_id(ast, id_key_min);
     if (!NIL_P(min) && NUM2LONG(min) == 0) {
-      if (onibi_ast_has_capture(atom)) *nullable_capture = 1;
+      if (atom_has_capture) *nullable_capture = 1;
       return 1;
     }
     return atom_nullable;
   }
   if (type == ONIBI_AST_ABSENCE) {
     int ignored_capture = 0;
-    int body_nullable = onibi_ast_nullable_scan(onibi_hash_value_id(ast, id_key_body), &ignored_capture, nullable_absence);
+    int ignored_has_capture = 0;
+    int body_nullable = onibi_ast_nullable_scan(onibi_hash_value_id(ast, id_key_body), &ignored_capture, nullable_absence, &ignored_has_capture);
     if (body_nullable) *nullable_absence = 1;
     return 0;
   }
@@ -3763,7 +3768,7 @@ static int onibi_ast_nullable_scan(VALUE ast, int *nullable_capture, int *nullab
     VALUE children = onibi_hash_value_id(ast, id_key_children);
     if (!RB_TYPE_P(children, T_ARRAY)) return 0;
     for (long i = 0; i < RARRAY_LEN(children); i++)
-      if (!onibi_ast_nullable_scan(rb_ary_entry(children, i), nullable_capture, nullable_absence)) result = 0;
+      if (!onibi_ast_nullable_scan(rb_ary_entry(children, i), nullable_capture, nullable_absence, has_capture)) result = 0;
     return result;
   }
   if (type == ONIBI_AST_ALTERNATIVE) {
@@ -3771,11 +3776,11 @@ static int onibi_ast_nullable_scan(VALUE ast, int *nullable_capture, int *nullab
     VALUE branches = onibi_hash_value_id(ast, id_key_branches);
     if (!RB_TYPE_P(branches, T_ARRAY)) return 0;
     for (long i = 0; i < RARRAY_LEN(branches); i++)
-      if (onibi_ast_nullable_scan(rb_ary_entry(branches, i), nullable_capture, nullable_absence)) result = 1;
+      if (onibi_ast_nullable_scan(rb_ary_entry(branches, i), nullable_capture, nullable_absence, has_capture)) result = 1;
     return result;
   }
   if (type == ONIBI_AST_GROUP || type == ONIBI_AST_OPTION_SCOPE || type == ONIBI_AST_ATOMIC)
-    return onibi_ast_nullable_scan(onibi_hash_value_id(ast, id_key_body), nullable_capture, nullable_absence);
+    return onibi_ast_nullable_scan(onibi_hash_value_id(ast, id_key_body), nullable_capture, nullable_absence, has_capture);
   if (type == ONIBI_AST_LOOKAHEAD || type == ONIBI_AST_LOOKBEHIND ||
       type == ONIBI_AST_ANCHOR || type == ONIBI_AST_MATCH_RESET) return 1;
   return 0;
