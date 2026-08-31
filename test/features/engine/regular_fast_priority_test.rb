f23a5eb17f7fca5004665c8578c7cb9154355b06
Masahiro Ide <imasahiro9@gmail.com>
2026-08-31 18:52:32 +0900

    Add ordered frontier priority differential coverage

---
+++ test/features/engine/regular_fast_priority_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class RegularFastPriorityTest < Minitest::Test
+  CASES = [["a|ab", "ab"], ["ab|a", "ab"], ["a*", "aaa"],
+           ["a+", "aaa"], ["a*?a", "aaa"], ["a+?a", "aaa"]].freeze
+
+  def test_ordered_frontier_matches_mri_ranges
+    CASES.each do |pattern, subject|
+      regexp = Onibi::Regexp.new(pattern)
+      info = regexp.send(:__onibi_diagnostics__, subject)
+      expected = Regexp.new(pattern).match(subject)
+      expected_range = expected ? [expected.begin(0), expected.end(0)] : nil
+      assert_equal 0, info[:exec_kind], pattern
+      assert_equal 0, info[:dfs], pattern
+      assert_equal 0, info[:fallback], pattern
+      assert_equal expected_range, [info[:match_start], info[:match_end]], pattern
+    end
+  end
+end
