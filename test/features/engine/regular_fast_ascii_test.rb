0f52f31eaea2464ac1395ae1a63f225346cc7f05
Masahiro Ide <imasahiro9@gmail.com>
2026-08-31 18:38:21 +0900

    Cover regular fast ASCII execution paths

---
+++ test/features/engine/regular_fast_ascii_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class RegularFastAsciiTest < Minitest::Test
+  def diagnostics(pattern, subject, options = nil)
+    regexp = options.nil? ? Onibi::Regexp.new(pattern) : Onibi::Regexp.new(pattern, options)
+    [regexp, regexp.send(:__onibi_diagnostics__, subject)]
+  end
+
+  def test_regular_literal_uses_ordered_frontier
+    regexp, info = diagnostics("a", "ba")
+    assert info[:rseq]
+    assert_equal 0, info[:exec_kind]
+    assert_equal 0, info[:dfs]
+    assert_equal 1, info[:match_start]
+    assert_equal 2, info[:match_end]
+    assert regexp.match?("ba")
+  end
+
+  def test_regular_capture_does_not_enter_dfs
+    _regexp, info = diagnostics("(a)", "a")
+    assert_equal 0, info[:exec_kind]
+    assert_equal 0, info[:dfs]
+    assert_equal 0, info[:fallback]
+    assert_equal 0, info[:semantic_capture_count]
+  end
+
+  def test_any_respects_multiline_option
+    _plain, plain_info = diagnostics(".", "\n")
+    _multi, multi_info = diagnostics(".", "\n", Onibi::Regexp::MULTILINE)
+    assert_equal 0, plain_info[:status]
+    assert_equal 1, multi_info[:status]
+    assert_equal 0, plain_info[:dfs]
+    assert_equal 0, multi_info[:dfs]
+  end
+
+  def test_lazy_repeat_keeps_first_accept
+    _regexp, info = diagnostics("a+?", "aaa")
+    assert_equal [0, 1], [info[:match_start], info[:match_end]]
+    assert_equal 0, info[:dfs]
+  end
+end
