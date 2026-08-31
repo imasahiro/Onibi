aca3d5c5a64cc085471f0a411da095cc0100862c
Masahiro Ide <imasahiro9@gmail.com>
2026-08-31 18:53:10 +0900

    Expose fallback counts for every executor

---
+++ test/features/engine/executor_dispatch_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ExecutorDispatchTest < Minitest::Test
+  def test_dynamic_fallback_is_visible
+    regexp = Onibi::Regexp.new("(a)\\1")
+    info = regexp.send(:__onibi_diagnostics__, "aa")
+    assert_equal 2, info[:exec_kind]
+    assert_equal 1, info[:dynamic]
+    assert_equal 1, info[:dfs]
+    assert_equal 1, info[:fallback]
+  end
+
+  def test_regular_unsupported_path_never_enters_dfs
+    regexp = Onibi::Regexp.new("(a)")
+    info = regexp.send(:__onibi_diagnostics__, "a")
+    assert_equal 0, info[:exec_kind]
+    assert_equal 0, info[:dfs]
+    assert_equal 0, info[:fallback]
+  end
+end
