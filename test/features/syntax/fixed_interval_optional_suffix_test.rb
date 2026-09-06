d8b325218d49d31f41fca4af799daab25156fcc8
Masahiro Ide <imasahiro9@gmail.com>
2026-09-06 23:21:45 +0900

    test(parser): prove fixed interval optional suffix semantics

--- test/features/syntax/fixed_interval_optional_suffix_test.rb
+++ test/features/syntax/fixed_interval_optional_suffix_test.rb
@@ -3,52 +3,44 @@
 require "test_helper"
 
 class FixedIntervalOptionalSuffixTest < Minitest::Test
-  def test_fixed_interval_question_mark_is_a_greedy_optional_exact_repeat
-    [[2, 0..3], [9, 0..10]].each do |count, lengths|
-      pattern = "a{#{count}}?"
+  BOUNDS = [2, 9].freeze
 
-      lengths.each do |length|
-        assert_native_match(pattern, "a" * length)
+  def test_optional_exact_and_lazy_exact_consumption_match_mri
+    BOUNDS.each do |count|
+      assert_native_match("\\Aa{#{count}}?\\z", "")
+      assert_native_match("\\Aa{#{count}}?\\z", "a" * count)
+      assert_native_match("\\Aa{#{count},#{count}}?\\z", "")
+      assert_native_match("\\Aa{#{count},#{count}}?\\z", "a" * count)
+    end
+  end
+
+  def test_optional_exact_and_lazy_exact_suffix_paths_match_mri
+    BOUNDS.each do |count|
+      optional = "\\Aa{#{count}}?b\\z"
+      lazy_exact = "\\Aa{#{count},#{count}}?b\\z"
+
+      ["b", "#{"a" * count}b", "#{"a" * count}c"].each do |subject|
+        assert_native_match(optional, subject)
+        assert_native_match(lazy_exact, subject)
       end
     end
   end
 
-  def test_equal_range_question_mark_stays_a_lazy_exact_repeat
-    [[2, 0..3], [9, 0..10]].each do |count, lengths|
-      pattern = "a{#{count},#{count}}?"
+  def test_optional_exact_and_lazy_exact_nullable_captures_match_mri
+    BOUNDS.each do |count|
+      atom = "あ"
+      optional = "\\A(#{atom}?){#{count}}?\\z"
+      lazy_exact = "\\A(#{atom}?){#{count},#{count}}?\\z"
+      optional_with_suffix = "\\A(#{atom}?){#{count}}?#{atom}\\z"
+      lazy_exact_with_suffix = "\\A(#{atom}?){#{count},#{count}}?#{atom}\\z"
 
-      lengths.each do |length|
-        assert_native_match(pattern, "a" * length)
+      ["", atom].each do |subject|
+        assert_native_match(optional, subject)
+        assert_native_match(lazy_exact, subject)
       end
-    end
-  end
-
-  def test_equal_range_lazy_repeat_keeps_exact_sequence_semantics
-    [2, 9].each do |count|
-      pattern = "a{#{count},#{count}}?a"
-
-      ["", "a", "a" * count, "a" * (count + 1)].each do |subject|
-        assert_native_match(pattern, subject)
-      end
-    end
-  end
-
-  def test_optional_exact_repeat_keeps_sequence_and_capture_semantics
-    [2, 9].each do |count|
-      pattern = "(a){#{count}}?a"
-
-      ["", "a", "a" * count, "a" * (count + 1)].each do |subject|
-        assert_native_match(pattern, subject)
-      end
-    end
-  end
-
-  def test_optional_exact_repeat_keeps_nullable_capture_semantics
-    [2, 9].each do |count|
-      pattern = "(a?){#{count}}?"
-
-      ["", "a", "a" * count].each do |subject|
-        assert_native_match(pattern, subject)
+      [atom, atom * 2].each do |subject|
+        assert_native_match(optional_with_suffix, subject)
+        assert_native_match(lazy_exact_with_suffix, subject)
       end
     end
   end
@@ -57,18 +49,27 @@ class FixedIntervalOptionalSuffixTest < Minitest::Test
 
   def assert_native_match(pattern, subject)
     expected = ::Regexp.new(pattern).match(subject)
-    actual = Onibi::Regexp.new(pattern).match(subject)
+    info = Onibi::Regexp.new(pattern).send(:__onibi_diagnostics__, subject)
+    label = [pattern, subject]
 
-    if expected.nil?
-      assert_nil actual, [pattern, subject.length]
-      return
+    assert info[:rseq], label
+    assert_equal 1, info[:exec_kind], label
+    assert_equal expected.nil? ? 0 : 1, info[:status], label
+    if expected
+      assert_equal expected.bytebegin(0), info[:match_start], label
+      assert_equal expected.byteend(0), info[:match_end], label
+      assert_equal capture_ranges(expected), info[:captures], label
     end
+    assert_operator info[:tagged], :>, 0, label
+    assert_equal 0, info[:regular], label
+    assert_equal 0, info[:dynamic], label
+    assert_equal 0, info[:dfs], label
+    assert_equal 0, info[:fallback], label
+  end
 
-    assert_equal expected.to_a, actual&.to_a, [pattern, subject.length]
-    assert_equal expected.offset(0), actual&.offset(0), [pattern, subject.length]
-
-    (1...expected.size).each do |index|
-      assert_equal expected.offset(index), actual&.offset(index), [pattern, subject.length, index]
+  def capture_ranges(match)
+    (1...match.size).map do |index|
+      [match.bytebegin(index) || -1, match.byteend(index) || -1]
     end
   end
 end
